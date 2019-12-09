7ff7b08dcab80ad9d12c676cb3c71e024f39dda2
sill2673 <sill2673@stud.kea.dk>
2019-12-09 16:46:59 +0100

    kontakt + anmeldelser + rettelser

---
+++ script.js
@@ -0,0 +1,54 @@
+  let indhold;
+  document.addEventListener("DOMContentLoaded", hentJson);
+
+  async function hentJson() {
+      const url = "https://marielouiselarsen.eu/kea/2-semester/tema10_eksamen/wordpress/wp-json/wp/v2/tatovering";
+      const myJson = await fetch(url);
+      indhold = await myJson.json();
+
+      visIndhold();
+  }
+
+  function visIndhold() {
+      document.querySelector(".tat_billede").src = indhold.billede.guid;
+  }
+
+
+  window.addEventListener("load", sidenVises);
+
+  function sidenVises() {
+      console.log("sidenVises");
+      document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+  }
+
+  function toggleMenu() {
+      console.log("toggleMenu");
+      document.querySelector("#menu").classList.toggle("hidden");
+
+      let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+      if (erSkjult == true) {
+          document.querySelector("#menuknap").textContent = "☰";
+      } else {
+          document.querySelector("#menuknap").textContent = "✖";
+      }
+  }
+
+  /* slideshow */
+
+  var slideIndex = 0;
+  carousel();
+
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {
+          slideIndex = 1
+      }
+      x[slideIndex - 1].style.display = "block";
+      setTimeout(carousel, 5000); // Change image every 5 seconds
+  }
